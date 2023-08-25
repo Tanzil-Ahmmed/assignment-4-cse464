ea36c6115526bcc9968fe9d7e22a90fae9328e23
Tanzil Ahmmed <106625031+Tanzil-Ahmmed@users.noreply.github.com>
2023-08-25 20:58:35 +0600

    Add files via upload

--- Home.dart
+++ Home.dart
@@ -1,7 +1,8 @@
 import 'package:calculator/widgets/button.dart';
 import 'package:calculator/widgets/colors.dart';
-import 'package:calculator/widgets/textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -17,10 +18,27 @@ class _HomeState extends State<Home> {
   var output = '';
 
   onButtonClick(value){
-    if (value == 'AC'){
+    if (value == 'c'){
       input = '';
       output = '';
     }
+    else if(value == 'v'){
+      input = input.substring(0, input.length -1);
+    }
+    else if (value == '='){
+      var userInput = input;
+      Parser p = Parser();
+      Expression expression = p.parse(userInput);
+      ContextModel cm = ContextModel();
+      var finalValue = expression.evaluate(EvaluationType.REAL, cm);
+
+      output = finalValue.toString();
+    }
+    else {
+      input = input + value ;
+    }
+
+    setState(() {});
   }
 
 
@@ -44,8 +62,6 @@ class _HomeState extends State<Home> {
         children: [
           //.................Screen.......................
 
-          // const CustomTextField(),
-
           Expanded(
             child: Container(
               width: double.infinity,
@@ -55,14 +71,12 @@ class _HomeState extends State<Home> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  Text(input, style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.w300)),
-                  Text(output,style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold)),
+                  Text(input, style: const TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold)),
+                  Text(output,style: const TextStyle(color: Colors.white, fontSize: 35, fontWeight: FontWeight.bold)),
                 ],
               ),
             ),
           ),
-
-          //const Spacer(),
 
 
           //...............................................Box..............................................
@@ -76,39 +90,53 @@ class _HomeState extends State<Home> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Row(
+                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Button1(label:'v', textColor: AppColors.boxColor,), Button1(label:'c', textColor: AppColors.boxColor), Button1(label: 'x' , textColor: AppColors.boxColor), Button1(label: '/', textColor: AppColors.boxColor)
+                    Button1(label:'v', textColor: AppColors.boxColor,onButtonPressed: onButtonClick,),
+                    Button1(label:'c', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,),
+                    Button1(label: 'x' , textColor: AppColors.boxColor, onButtonPressed: onButtonClick,),
+                    Button1(label: '/', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,)
                   ],
                 ),
-                const Row(
+                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Button1(label:'(', textColor: AppColors.boxColor), Button1(label:')', textColor: AppColors.boxColor), Button1(label: '%', textColor: AppColors.boxColor), Button1(label: '*', textColor: AppColors.boxColor)
+                    Button1(label:'(', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,),
+                    Button1(label:')', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,),
+                    Button1(label: '%', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,),
+                    Button1(label: '*', textColor: AppColors.boxColor,onButtonPressed: onButtonClick,)
                   ],
                 ),
-                const Row(
+                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Button1(label:'1', textColor: AppColors.boxColor), Button1(label:'2', textColor: AppColors.boxColor), Button1(label: '3', textColor: AppColors.boxColor), Button1(label: '-', textColor: AppColors.boxColor)
+                    Button1(label:'1', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,),
+                    Button1(label:'2', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,),
+                    Button1(label: '3', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,),
+                    Button1(label: '-', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,)
                   ],
                 ),
-                const Row(
+                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Button1(label:'4', textColor: AppColors.boxColor), Button1(label:'5', textColor: AppColors.boxColor), Button1(label: '6', textColor: AppColors.boxColor), Button1(label: '+', textColor: AppColors.boxColor)
+                    Button1(label:'4', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,),
+                    Button1(label:'5', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,),
+                    Button1(label: '6', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,),
+                    Button1(label: '+', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,)
                   ],
                 ),
                 Row(
                   children: [
-                    const Expanded(
+                     Expanded(
                       child: Column(
                         children: [
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Button1(label:'7', textColor: AppColors.boxColor), Button1(label:'8', textColor: AppColors.boxColor), Button1(label: '9', textColor: AppColors.boxColor)
+                              Button1(label:'7', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,),
+                              Button1(label:'8', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,),
+                              Button1(label: '9', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,)
                             ],
                           ),
                           SizedBox(height: 20,),
@@ -116,7 +144,9 @@ class _HomeState extends State<Home> {
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Button1(label:'0', textColor: AppColors.boxColor), Button1(label:'00', textColor: AppColors.boxColor), Button1(label: '.', textColor: AppColors.boxColor)
+                              Button1(label:'0', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,),
+                              Button1(label:'00', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,),
+                              Button1(label: '.', textColor: AppColors.boxColor, onButtonPressed: onButtonClick,)
                             ],
                           ),
                         ],
@@ -127,9 +157,9 @@ class _HomeState extends State<Home> {
                       height: 160,
                       width: 70,
                       decoration: const BoxDecoration(
-                          color: AppColors.buttonColor
+                        color: AppColors.buttonColor
                       ),
-                      child: const Button1(label: '=',textColor: AppColors.buttonColor),
+                      child: Button1(label: '=',textColor: AppColors.buttonColor, onButtonPressed: onButtonClick,),
                     )
                   ],
                 )
